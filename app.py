f1bff384d35e5ecc8c902d5ebb62c3f11714a698
Jesse <jessevrieling@gmail.com>
2024-03-14 13:24:22 +0100

    Added content to homepage

--- app.py
+++ app.py
@@ -1,14 +1,12 @@
 from flask import Flask, render_template
-import sqlite3
 
 app = Flask(__name__)
-con = sqlite3.connect("database.db")
 
 @app.route("/", methods=["get"])
 def index():
     return render_template("index.html")
 
-@app.route("/inloggen")
+@app.route("/inloggen", methods=["get", "post"])
 def inloggen():
     return render_template("login.html")
 
