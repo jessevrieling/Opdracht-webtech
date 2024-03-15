21d15e58b888200f35fd9f4bcb5382c25ffd0190
Jesse <jessevrieling@gmail.com>
2024-03-15 09:50:30 +0100

    Content added

--- app.py
+++ app.py
@@ -1,14 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/", methods=["get"])
+@app.route("/", methods=["GET"])
 def index():
     return render_template("index.html")
 
-@app.route("/inloggen", methods=["get", "post"])
+@app.route("/inloggen", methods=["GET", "POST"])
 def inloggen():
-    return render_template("login.html")
+    if request.method == "GET":
+        return render_template("login.html")
+    elif request.method == "POST":
+        name = request.form.get("username")
+        password = request.form.get("password")
+        print(name, password)
+        return render_template("login.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
