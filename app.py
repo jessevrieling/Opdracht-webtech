d7c09a90f826176f20b4fc7db6893e7eea3f7296
Jesse <jessevrieling@gmail.com>
2024-03-18 18:05:35 +0100

    Password check added

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 import sqlite3
+import bcrypt
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
-con = sqlite3.connect("database.db")
 
 @app.route("/", methods=["GET"])
 def index():
@@ -13,9 +13,23 @@ def inloggen():
     if request.method == "GET":
         return render_template("login.html")
     elif request.method == "POST":
+        con = sqlite3.connect("database.db")
+        cursor = con.cursor()
+
         name = request.form.get("username")
         password = request.form.get("password")
-        return render_template("login.html")
+        
+        bytes = password.encode("utf-8")
+        salt = bcrypt.gensalt()
+        passwordHash = bcrypt.hashpw(bytes, salt)
+        query = f"SELECT COUNT(*) FROM users WHERE name=\"{name}\" AND password=\"{passwordHash}\""
+        cursor.execute(query)
+        result = cursor.fetchone()
+
+        if result[0] == 1:
+            return render_template("boeken.html")
+        else:
+            return "<h1>Fout</h1>"
 
 @app.route("/boeken")
 def boekingen():
