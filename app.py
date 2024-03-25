96efaed1c3235212c07fa329bb6d3942ebdb6e09
Jesse <jessevrieling@gmail.com>
2024-03-25 16:59:15 +0100

    Login backend added

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template, request
+import sqlite3
+import bcrypt
 
 app = Flask(__name__)
 
@@ -11,9 +13,23 @@ def inloggen():
     if request.method == "GET":
         return render_template("login.html")
     elif request.method == "POST":
+        con = sqlite3.connect("database.db")
+        cursor = con.cursor()
+
         name = request.form.get("username")
         password = request.form.get("password")
-        return render_template("login.html")
+        
+        bytes = password.encode("utf-8")
+        salt = bcrypt.gensalt()
+        passwordHash = bcrypt.hashpw(bytes, salt)
+        query = f"SELECT COUNT(*) FROM users WHERE name=\"{name}\" AND password=\"{passwordHash}\""
+        cursor.execute(query)
+        result = cursor.fetchone()
+
+        if result[0] == 1:
+            return "<h1>Goed</h1"
+        else:
+            return "<h1>Fout</h1>"
 
 @app.route("/registreren", methods=["GET"])
 def registreren():
