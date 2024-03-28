d4f5c9a25236f724fb06cc005b5e90d48fa877e9
Jesse <jessevrieling@gmail.com>
2024-03-28 10:48:49 +0100

    Backend fix

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, session
 from flask_bcrypt import Bcrypt
 import sqlite3
+from secrets import token_hex
 
 app = Flask(__name__)
+app.secret_key = token_hex(16)
 hasher = Bcrypt()
 
 @app.route("/", methods=["GET"])
@@ -12,25 +14,37 @@ def index():
 @app.route("/inloggen", methods=["GET", "POST"])
 def inloggen():
     if request.method == "GET":
-        return render_template("login.html")
+        if session.get("loggedIn") == True:
+            return redirect("/mijnboekingen")
+        else:
+            return render_template("login.html", errorCode="")
     elif request.method == "POST":
         con = sqlite3.connect("database.db")
         cursor = con.cursor()
 
         identifier = request.form.get("identifier")
         plainPassword = request.form.get("password")
-        
+
         query = f"SELECT password FROM users WHERE name=\"{identifier}\" OR email=\"{identifier}\""
         cursor.execute(query)
         result = cursor.fetchone()
-        con.close()
-        
-        hashedPassword = result[0]
 
-        if hasher.check_password_hash(hashedPassword, plainPassword):
-            return "<h1>goed</h1>"
+        if result is None:
+            return render_template("login.html", errorCode="Onjuist wachtwoord of e-mail")
         else:
-            return "<h1>fout</h1>"
+            passwordHash = result[0]
+
+            if hasher.check_password_hash(passwordHash, plainPassword):
+                query = f"SELECT name FROM users WHERE name=\"{identifier}\" OR email=\"{identifier}\""
+                cursor.execute(query)
+                username = cursor.fetchone()[0]
+
+                session["loggedIn"] = True
+                session["username"] = username
+                con.close()
+                return redirect("/mijnboekingen")
+            else:
+                return render_template("login.html", errorCode="Onjuist wachtwoord of e-mail")
         
 @app.route("/registreren", methods=["GET", "POST"])
 def registreren():
@@ -45,15 +59,21 @@ def registreren():
         if password == passwordConfirm:
             con = sqlite3.connect("database.db")
             cursor = con.cursor()
-            passwordHash = hasher.generate_password_hash(password).decode('utf-8')
-
-            query = f"INSERT INTO users (email, name, password) VALUES(\"{email}\", \"{name}\", \"{passwordHash}\")"
+            query = f"SELECT COUNT(*) FROM users WHERE email=\"{email}\""
             cursor.execute(query)
-            con.commit()
-            con.close()
-            return redirect("/Gefeliciteerd!")
+
+            if str(cursor.fetchone()[0]) == "0":
+                passwordHash = hasher.generate_password_hash(password).decode('utf-8')
+
+                query = f"INSERT INTO users (email, name, password) VALUES(\"{email}\", \"{name}\", \"{passwordHash}\")"
+                cursor.execute(query)
+                con.commit()
+                con.close()
+                return redirect("/Gefeliciteerd!")
+            else:
+                return render_template("registreren.html", errorCode="E-mail al in gebruik")
         else:
-            return "Wachtwoord komt niet overeen"
+            return render_template("registreren.html", errorCode="Wachtwoord komt niet overeen")
 
 @app.route("/Gefeliciteerd!", methods=["GET", "POST"])
 def aangemeld():
