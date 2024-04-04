8461fd2988ed63f938de1a1e14c0675c0faeb7d8
Jesse <jessevrieling@gmail.com>
2024-04-04 08:41:59 +0200

    Rendering houses

--- setup/database/setupScript.sql
+++ setup/database/setupScript.sql
@@ -9,12 +9,17 @@ CREATE TABLE IF NOT EXISTS users (
 
 CREATE TABLE IF NOT EXISTS houses (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
+    image_url TEXT NOT NULL,
+    title TEXT NOT NULL,
+    description TEXT NOT NULL,
     capacity INTEGER NOT NULL,
     price INTEGER NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS reservations (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
+    date_arrival DATE NOT NULL,
+    date_departure DATE NOT NULL,
     userId INTEGER,
     houseId INTEGER,
     FOREIGN KEY(userId) REFERENCES users(id),
